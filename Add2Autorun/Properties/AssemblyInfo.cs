868de6fa5dde1c70c3b75ad6f8cf8078f3615f19
Andrey Belyaev <andrey.belyaev@pag.company>
2018-03-01 12:06:02 +0300

    Added assembly info

--- Add2Autorun/Properties/AssemblyInfo.cs
+++ Add2Autorun/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Add2Autorun")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("https://github.com/Svetomech/Add2Autorun")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Svetomech")]
 [assembly: AssemblyProduct("Add2Autorun")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © Andrey Belyaev 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
