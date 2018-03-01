47c74c2f63e30a0a8b0008f322c884650ea0eb39
Andrey Belyaev <andrey.belyaev@pag.company>
2018-03-01 11:32:24 +0300

    Revert "Fixed association types"
    This reverts commit 58d6fc323d8a456053f70fe96f6a5372396c2e93.

--- Add2Autorun/AddToAutorunExtension.cs
+++ Add2Autorun/AddToAutorunExtension.cs
@@ -14,7 +14,7 @@ namespace Add2Autorun
     /// </summary>
     /// <seealso cref="SharpContextMenu" />
     [ComVisible(true)]
-    [COMServerAssociation(AssociationType.ClassOfExtension, ".exe")]
+    [COMServerAssociation(AssociationType.AllFiles)]
     public class AddToAutorunExtension : SharpContextMenu
     {
         /// <summary>
